6c28920dc9ef42601f5edb0eb5c13b9fb58ddc6c
Andre <3oliva41@solent.ac.uk>
2020-03-10 22:18:27 +0000

    getauctions route retrieves dummy auction data now

--- mainproject/project/web/src/main/java/org/solent/com504/project/impl/rest/RestService.java
+++ mainproject/project/web/src/main/java/org/solent/com504/project/impl/rest/RestService.java
@@ -9,7 +9,11 @@ package org.solent.com504.project.impl.rest;
  *
  * @author gallenc
  */
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -22,7 +26,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.solent.com504.project.model.auction.dto.Auction;
 import org.solent.com504.project.model.auction.service.AuctionService;
+import org.solent.com504.project.model.bid.dto.Bid;
 import org.solent.com504.project.model.dto.ReplyMessage;
+import org.solent.com504.project.model.lot.dto.Lot;
 import org.solent.com504.project.model.service.ServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -50,13 +56,13 @@ public class RestService {
     ServiceFacade serviceFacade = null;
 
     @Autowired(required = true)
-    @Qualifier("serviceFacade")
+    @Qualifier("auctionService")
     AuctionService auctionService = null;
 
     /**
      * this is a very simple rest test message which only returns a string
      *
-     * http://localhost:8084/projectweb/rest/auctionService/
+     * http://localhost:8084/projectweb/rest/auctionService/getHeartbeat
      *
      * @return String simple message
      */
@@ -110,20 +116,56 @@ public class RestService {
      */
     @GET
     @Path("/getAuctions")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON})
     public Response getAuctions() {
 
         try {
+            
+            //Creating dummy data @@@.
+            
+            // ##### auction 1 ####
+            Lot lot1 = createRandomLot(createRandomBidListForLot());
+            Lot lot2 = createRandomLot(createRandomBidListForLot());
+            Lot lot3 = createRandomLot(createRandomBidListForLot());
+    
+            Set<Lot> lots = new HashSet();
+            lots.add(lot1);
+            lots.add(lot2);
+            lots.add(lot3);
 
+            Auction auction1 = new Auction();
+            auction1.setLots(lots);
+            
+
+            
+
+            // ##### auction 2 ####
+            Lot lot4 = createRandomLot(createRandomBidListForLot());
+            Lot lot5 = createRandomLot(createRandomBidListForLot());
+            Lot lot6 = createRandomLot(createRandomBidListForLot());
+    
+            Set<Lot> lots2 = new HashSet();
+            lots2.add(lot4);
+            lots2.add(lot5);
+            lots2.add(lot6);
+            Auction auction2 = new Auction();
+            auction2.setLots(lots2);
+            
+            //############# end ###############
+            
             ReplyMessage replyMessage = new ReplyMessage();
-
+            
+            List<Auction> auctionList = new ArrayList();
+            auctionList.add(auction1);
+            auctionList.add(auction2);
+            
+            replyMessage.setAuctionList(auctionList);
+            
             LOG.debug("/getAuctions called");
 
             if (auctionService == null) {
                 throw new RuntimeException("auctionService==null and has not been initialised");
             }
-
-            List<Auction> auctionList = auctionService.getAuctions();
             replyMessage.setAuctionList(auctionList);
             replyMessage.setCode(Response.Status.OK.getStatusCode());
             return Response.status(Response.Status.OK).entity(replyMessage).build();
@@ -142,5 +184,37 @@ public class RestService {
         }
 
     }
+    
+    private List<Bid> createRandomBidListForLot(){
+        List<Bid> bids = new ArrayList();
 
+        for (int i = 0; i < 10; i++){
+            Bid bid = new Bid();
+            Date date = new Date();
+            bid.setTimeStamp(date);
+            bid.setLot(null);
+            //just to test the filter inside the lot object shouldn't add a bid if the value is lower than the reserved price
+            bid.setValue(7.0 + i);
+            bids.add(bid);
+        }
+        
+        return bids;
+    }
+       
+    private Lot createRandomLot(List<Bid>bids){
+        Lot lot = new Lot();
+        lot.setDuration(1);
+        lot.setGrade("Test grade: " + (Math.random() * 20));
+        lot.setQuantity(10);
+        lot.setReservedPrice(10.0);
+        lot.setPickdate(null);
+        lot.setLife_days(12);
+        
+        for(int i = 0; i < bids.size(); i++){
+            lot.addBid(bids.get(i));
+        }
+        
+        return lot;
+    }
+   
 }
